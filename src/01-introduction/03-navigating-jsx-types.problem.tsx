b27bbab33f59ec8689e44a67356277b6f7d1ef9d
Masoud <masoud.zeinalabedini@gmail.com>
2024-07-10 10:46:08 +0330

    Finihs 03-navigating-jsx-types.problem

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,9 +2,9 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={1}
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={() => {}}
 
       // How do I get autocomplete with JSX?
     />
