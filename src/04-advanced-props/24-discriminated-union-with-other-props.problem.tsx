546cab8cf32cefdb1cbd3413d303486dcf852231
Masoud <masoud.zeinalabedini@gmail.com>
2024-07-15 12:13:52 +0330

    Fix 24-discriminated-union-with-other-props.problem

--- src/04-advanced-props/24-discriminated-union-with-other-props.problem.tsx
+++ src/04-advanced-props/24-discriminated-union-with-other-props.problem.tsx
@@ -1,11 +1,12 @@
-type ModalProps =
+type ModalProps = (
   | {
       variant: "no-title";
     }
   | {
       variant: "title";
       title: string;
-    };
+    }
+) & { buttonColor: string };
 
 /**
  * 1. How do we add a `buttonColor` prop to the `ModalProps` type that is
