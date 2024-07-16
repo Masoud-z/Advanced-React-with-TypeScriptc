fa4f32b99f169717933341571541acdcee066a5f
Masoud <masoud.zeinalabedini@gmail.com>
2024-07-16 11:25:07 +0330

    Finish 31-as-const.problem

--- src/04-advanced-props/31-as-const.problem.ts
+++ src/04-advanced-props/31-as-const.problem.ts
@@ -4,10 +4,12 @@ const BACKEND_TO_FRONTEND_STATUS_MAP = {
   0: "pending",
   1: "success",
   2: "error",
-};
+} as const;
 
-type BackendStatus = unknown;
-type FrontendStatus = unknown;
+type BackendStatusMap = typeof BACKEND_TO_FRONTEND_STATUS_MAP;
+
+type BackendStatus = keyof BackendStatusMap;
+type FrontendStatus = BackendStatusMap[keyof BackendStatusMap];
 
 type test = [
   Expect<Equal<BackendStatus, 0 | 1 | 2>>,
